b4d6b68b18faf109990dc797a4fcf49f19a94f60
Raghunath-Vit <raghunathsinghmannu1@gmail.com>
2024-12-17 02:57:05 +0530

    Code file added

--- HousePricePrediction.py
+++ HousePricePrediction.py
@@ -1,17 +1,60 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
 import pandas as pd
 import numpy as np
 
+
+# In[2]:
+
+
 df=pd.read_csv('House Price India.csv')
+
+
+# In[3]:
+
+
 df.head(5)
+
+
+# In[4]:
+
+
 pd.isnull(df).head()
+
+
+# In[5]:
+
+
 df.isna().sum()
+
+
+# In[6]:
+
+
 df.info()
+
+
+# In[7]:
+
+
 # returns a dataframe with column names of the dataset
 
 pd.DataFrame(list(df.columns), columns=['Column Name'])
 
+
+# In[8]:
+
+
 from matplotlib import pyplot as plt
 import seaborn as sns
+
+
+# In[9]:
+
 
 # used to return a countplot with null title and figsize to be (15, 8) as default
 
@@ -21,6 +64,10 @@ def countplot(dataframe, x_val, plot_title='', figsize=(15,8)):
     sns.countplot(data=df, x=x_val)
     plt.show()
 
+
+# In[10]:
+
+
 # used to return a barplot
 
 def barplot(dataframe, x_val, y_val):
@@ -28,28 +75,56 @@ def barplot(dataframe, x_val, y_val):
     plt.title(x_val.title() + ' vs ' + y_val.title())
     plt.show()
 
+
+# In[11]:
+
+
 df.shape
+
+
+# In[12]:
+
 
 #Univariate Analysis
 # returns the countplot of bedrooms
 
 countplot(dataframe=df, x_val='number of bedrooms', plot_title='Count of Number of Bedrooms')
 
+
+# In[13]:
+
+
 # returns the countplot of bathrooms
 
 countplot(dataframe=df, x_val='number of bathrooms', plot_title='Count of Number of Bathrooms')
+
+
+# In[14]:
+
 
 # returns the countplot of floors
 
 countplot(dataframe=df, x_val='number of floors', figsize=(8,5), plot_title='Count of Number of Floors')
 
+
+# In[15]:
+
+
 # returns the countplot of number water present or not
 
 countplot(dataframe=df, x_val='waterfront present', figsize=(8,6), plot_title='Water present (1) or not (0)')
 
+
+# In[16]:
+
+
 # returns the countplot of views
 
 countplot(dataframe=df, x_val='number of views', figsize=(8,5), plot_title='Number of Views')
+
+
+# In[17]:
+
 
 plt.figure(figsize=(10,10))
 sns.jointplot(x=df.Lattitude.values, y=df.Longitude.values,size=10)
@@ -58,32 +133,59 @@ plt.xlabel('Latitude',fontsize=12)
 plt.show()
 sns.despine
 
+
+# In[18]:
+
+
 # returns the countplot of House Condition
 
 countplot(dataframe=df, x_val='condition of the house', figsize=(8,5), plot_title='House Condition')
 
+
+# In[19]:
+
+
 # returns the countplot of each grade of house
 
 countplot(dataframe=df, x_val='grade of the house', figsize=(8,5), plot_title='Grade of the House')
+
+
+# In[20]:
+
 
 # returns the countplot of Scools nearby
 
 countplot(dataframe=df, x_val='Number of schools nearby', figsize=(8,5), plot_title='Number of Schools Nearby')
 
 
+# In[21]:
+
+
 sns.displot(df['Price'])
 plt.title('Price Distribution')
 plt.show()
+
+
+# In[22]:
+
 
 # returns the histplot of distance from airport
 
 sns.histplot(data=df, x='Distance from the airport')
 plt.show()
 
+
+# In[23]:
+
+
 #Bi - variate Analysis
 sns.barplot(data=df, x='Number of schools nearby', y='Price')
 plt.title('Number of schools vs Price')
 plt.show()
+
+
+# In[24]:
+
 
 sns.barplot(data=df, x='number of bathrooms', y='Price')
 plt.suptitle('Number of bathrooms vs Price')
@@ -91,15 +193,31 @@ plt.tight_layout()
 plt.show()
 # barplot(dataframe=df, x_val='number of bathrooms', y_val='Price')
 
+
+# In[25]:
+
+
 barplot(dataframe=df, x_val='number of bedrooms', y_val='Price')
 
+
+# In[26]:
+
+
 barplot(dataframe=df, x_val='grade of the house', y_val='Price')
+
+
+# In[27]:
+
 
 # Visualize the distribution of each numerical variable
 df.hist(bins=20, figsize=(15, 10))
 plt.suptitle('Histograms of Numerical Variables', y=1.02)  # Adjust the y-coordinate to prevent overlapping
 plt.tight_layout()  # Automatically adjusts subplot parameters for better layout
 plt.show()
+
+
+# In[28]:
+
 
 # Visualize correlation matrix using a heatmap
 correlation_matrix = df.corr()
@@ -110,11 +228,19 @@ plt.suptitle('Correlation Matrix', y=1.02)
 plt.tight_layout()
 plt.show()
 
+
+# In[29]:
+
+
 # Pairplot to visualize relationships between numerical variables
 sns.pairplot(df[['number of bedrooms', 'living area', 'lot area', 'number of floors', 'Price']])
 plt.suptitle('Pairplot of Numerical Variables', y=1.02)  # Adjust the y-coordinate to prevent overlapping
 plt.tight_layout()  # Automatically adjusts subplot parameters for better layout
 plt.show()
+
+
+# In[30]:
+
 
 # Boxplot to identify patterns and outliers
 plt.figure(figsize=(15, 8))
@@ -122,8 +248,16 @@ sns.boxplot(x='number of bedrooms', y='Price', data=df)
 plt.title('Boxplot of Price by Number of Bedrooms')
 plt.show()
 
+
+# In[31]:
+
+
 from datetime import datetime
 # from math import radians, sin, cos, sqrt, atan2
+
+
+# In[32]:
+
 
 # Feature engineering
 
@@ -153,6 +287,9 @@ df['floor_area_ratio'] = df['living area'] / df['lot area']
 print(df.head())
 
 
+# In[33]:
+
+
 correlation_matrix = df.corr()
 
 plt.figure(figsize=(24, 18))  # Adjusted figure size
@@ -162,6 +299,29 @@ plt.suptitle('Correlation Matrix', y=0.95)  # Adjusted y position of the title
 plt.tight_layout()  # Adjust layout to prevent overlapping
 plt.show()
 
+
+# In[34]:
+
+
+# # Save the updated DataFrame to a new CSV file
+# df.to_csv('updated_dataset.csv', index=False)
+
+
+# In[35]:
+
+
+# df.head()
+
+
+# In[36]:
+
+
+# pip install scikit-learn
+
+
+# In[37]:
+
+
 # Convert 'number of bathrooms' and 'number of floors' to integers
 df['number of bathrooms'] = df['number of bathrooms'].astype(int)
 df['number of floors'] = df['number of floors'].astype(int)
@@ -169,7 +329,15 @@ df['floor_area_ratio']=df['floor_area_ratio'].astype(int)
 df['basement_ratio']=df['basement_ratio'].astype(int)
 df['age_of_renovation']=df['age_of_renovation'].astype(int)
 
+
+# In[38]:
+
+
 df.info()
+
+
+# In[39]:
+
 
 # Calculate the correlation matrix
 correlation_matrix = df[['number of bedrooms', 'number of bathrooms', 'living area', 'lot area', 
@@ -191,7 +359,25 @@ price_correlation = correlation_matrix['Price'].sort_values(ascending=False)
 # Display the correlation values
 print(price_correlation)
 
+
+# In[40]:
+
+
+# # Get the column names
+# column_names = df.columns.tolist()
+
+# # Print the column names
+# print(column_names)
+
+
+# In[41]:
+
+
 df.describe()
+
+
+# In[42]:
+
 
 # #Splitting the dataset
 # from sklearn.model_selection import train_test_split
@@ -208,7 +394,15 @@ df = df.drop(columns=['id', 'Date'])
 # Print the first few rows to verify the changes
 print(df.head())
 
+
+# In[43]:
+
+
 df.head()
+
+
+# In[44]:
+
 
 # import seaborn as sns
 # plt.figure(figsize=(12,10))
@@ -231,24 +425,43 @@ sns.heatmap(correlation_matrix, annot=True, cmap=plt.cm.CMRmap_r)
 # Show the plot
 plt.show()
 
+
+# In[46]:
+
+
 def correlation(dataset, threshold):
     col_corr = set()
     corr_matrix = dataset.corr()
     for i in range(len(corr_matrix.columns)):
         for j in range(i):
-              if corr_matrix.iloc[i, j] > threshold:
-#             if corr_matrix.iloc[i, j] > threshold or corr_matrix.iloc[i, j] < -threshold:
+            if corr_matrix.iloc[i, j] > threshold or corr_matrix.iloc[i, j] < -threshold:
                 colname = corr_matrix.columns[i]
                 col_corr.add(colname)
     return col_corr
 
+
+# In[47]:
+
+
 corr_features=correlation(df,0.7)
 len(set(corr_features))
 
+
+# In[48]:
+
+
 corr_features
+
+
+# In[49]:
+
 
 corr_features.discard('Price')
 corr_features
+
+
+# In[50]:
+
 
 # Drop the correlated features from the DataFrame
 df = df.drop(corr_features,axis=1)
@@ -256,16 +469,28 @@ df = df.drop(corr_features,axis=1)
 # Save the modified DataFrame to a new CSV file
 df.to_csv("modified_dataset.csv", index=False)
 
+
+# In[51]:
+
+
 # Load the modified CSV file into a DataFrame
 modified_df = pd.read_csv("modified_dataset.csv")
 
 # Display the head of the modified DataFrame
 modified_df.head()
 
+
+# In[52]:
+
+
 modified_df = modified_df.drop(columns=['Lattitude', 'Longitude','Postal Code'])
 
 # Save the modified DataFrame to a new CSV file
 modified_df.to_csv("modified_dataset.csv", index=False)
+
+
+# In[53]:
+
 
 # modified_df = modified_df.drop(columns=['Postal Code'])
 
@@ -273,14 +498,38 @@ modified_df.to_csv("modified_dataset.csv", index=False)
 # modified_df.to_csv("modified_dataset.csv", index=False)
 modified_df.head()
 
+
+# In[54]:
+
+
 # Separate features (X) and target variable (y)
 X = modified_df.drop('Price', axis=1)
 y = modified_df['Price']
+
+
+# In[55]:
+
+
+X
+
+
+# In[56]:
+
+
+y
+
+
+# In[57]:
+
 
 modified_df = modified_df.drop(columns=['Built Year','Renovation Year','basement_ratio','Distance from the airport','Number of schools nearby','floor_area_ratio','condition of the house','Area of the basement'])
 
 # Save the modified DataFrame to a new CSV file
 modified_df.to_csv("modified_dataset.csv", index=False)
+
+
+# In[58]:
+
 
 # modified_df = modified_df.drop(columns=['Renovation Year'])
 
@@ -288,10 +537,25 @@ modified_df.to_csv("modified_dataset.csv", index=False)
 # modified_df.to_csv("modified_dataset.csv", index=False)
 modified_df.head()
 
+
+# In[ ]:
+
+
+
+
+
+# In[59]:
+
+
 from sklearn.model_selection import train_test_split
 
 # Split the data into train and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+
+# In[60]:
+
 
 from sklearn.preprocessing import StandardScaler
 
@@ -313,17 +577,48 @@ y_train_scaled = target_scaler.fit_transform(y_train.values.reshape(-1, 1)).flat
 # Transform the testing target variable
 y_test_scaled = target_scaler.transform(y_test.values.reshape(-1, 1)).flatten()
 
+
+# In[61]:
+
+
+# #Features Scaling:- i am using Standardization because it's optimal for all case while Normalization preferred when the value is near the mean values 
+# from sklearn.preprocessing import StandardScaler
+# scaler=StandardScaler()
+# # Fit the scaler to the training data and transform the training data
+# X_train_scaled = scaler.fit_transform(X_train)
+
+# # Transform the testing data using the scaler fitted on the training data
+# X_test_scaled = scaler.transform(X_test)
+
+
+
+# In[62]:
+
+
 X_train_scaled
 
+
+# In[63]:
+
+
 X_test_scaled
+
+
+# In[64]:
+
 
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, VotingRegressor
 from sklearn.svm import SVR
 from sklearn.linear_model import LinearRegression
 from xgboost import XGBRegressor
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
+
+
+
+# In[65]:
+
 
 from sklearn.ensemble import RandomForestRegressor
 from xgboost import XGBRegressor
@@ -345,17 +640,11 @@ y_pred_xgb = xgboost_model.predict(X_test_scaled)
 y_pred_rf
 y_pred_xgb
 
-# # Combine predictions using averaging
-# ensemble_predictions = (y_pred_rf + y_pred_xgb) / 2
 
-# # Evaluate ensemble model
-# mse = mean_squared_error(y_test, ensemble_predictions)
-# mae = mean_absolute_error(y_test, ensemble_predictions)
-# r2 = r2_score(y_test, ensemble_predictions)
 
-# print("Mean Squared Error (MSE):", mse)
-# print("Mean Absolute Error (MAE):", mae)
-# print("R-squared:", r2)
+
+# In[66]:
+
 
 # Evaluate Random Forest model
 r2_rf = r2_score(y_test, y_pred_rf)
@@ -369,13 +658,27 @@ print("R-squared:", r2_rf)
 print("\nXGBoost Model:")
 print("R-squared:", r2_xgb)
 
+
+# In[67]:
+
+
 # Combine predictions using averaging
 ensemble_predictions = (y_pred_rf + y_pred_xgb) / 2
 
 # Evaluate ensemble model
-r2 = r2_score(y_test, ensemble_predictions)
+r2_rf_xgb = r2_score(y_test, ensemble_predictions)
 
-print("R-squared:", r2)
+print("R-squared:", r2_rf_xgb)
+
+
+# In[68]:
+
+
+# !pip install catboost
+
+
+# In[69]:
+
 
 from sklearn.ensemble import RandomForestRegressor
 from xgboost import XGBRegressor
@@ -408,10 +711,14 @@ print("R-Squared: ",r_cb)
 ensemble_predictions = (y_pred_rf + y_pred_xgb + y_pred_cb) / 3
 
 # Evaluate ensemble model
-r2 = r2_score(y_test, ensemble_predictions)
+r2_rf_xgb_cb = r2_score(y_test, ensemble_predictions)
 
 print("Ensemble Model:")
-print("R-squared:", r2)
+print("R-squared:", r2_rf_xgb_cb)
+
+
+# In[70]:
+
 
 from sklearn.ensemble import RandomForestRegressor
 from xgboost import XGBRegressor
@@ -447,10 +754,14 @@ print("R-squared:", r2_xg)
 ensemble_predictions = (y_pred_rf + y_pred_xgb + y_pred_cb) / 3
 
 # Evaluate ensemble model
-r2 = r2_score(y_test, ensemble_predictions)
+r2_2 = r2_score(y_test, ensemble_predictions)
 
 print("Ensemble Model:")
-print("R-squared:", r2)
+print("R-squared:", r2_2)
+
+
+# In[71]:
+
 
 # Define a threshold (e.g., mean of the target variable)
 threshold = y_train.mean()
@@ -471,19 +782,26 @@ recall = true_positives / (true_positives + false_negatives)
 print("Precision:", precision)
 print("Recall:", recall)
 
-import pickle
+
+# In[72]:
+
+
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, VotingRegressor
 from sklearn.svm import SVR
-from sklearn.linear_model import LinearRegression
-from xgboost import XGBRegressor  
+from sklearn.linear_model import Ridge, Lasso
+from xgboost import XGBRegressor
+from catboost import CatBoostRegressor
 from sklearn.metrics import r2_score
-from sklearn.model_selection import train_test_split
 
 # Define individual base models
 models = {
     'Random Forest': RandomForestRegressor(),
     'XGBoost': XGBRegressor(),
-    'CatBoost': CatBoostRegressor(verbose=0)
+    'CatBoost': CatBoostRegressor(verbose=0),
+    'SVR': SVR(),
+    'Ridge': Ridge(),
+    'Lasso': Lasso(),
+    'Catboost+RandomForest_gb': RandomForestRegressor()  # Add Catboost+RandomForest_gb here
 }
 
 # Train individual base models and compute R-squared scores
@@ -493,39 +811,48 @@ for model_name, model in models.items():
     y_pred = model.predict(X_test)
     r2_scores[model_name] = r2_score(y_test, y_pred)
 
-# Evaluate ensemble model and compute its R-squared score
-y_pred_rf = random_forest_model.predict(X_test)
-y_pred_xgb = xgboost_model.predict(X_test)
-y_pred_cb = catboost_model.predict(X_test)
-ensemble_predictions = (y_pred_rf + y_pred_xgb + y_pred_cb) / 3
-ensemble_r2 = r2_score(y_test, ensemble_predictions)
-r2_scores['Ensemble'] = ensemble_r2
+# Add CatBoost R-squared score from the provided code
+r_cb = r2_score(y_test, y_pred_cb)
+r2_scores['CatBoost'] = r_cb
+
+#Adding Previous r2Score of different Combinations
+r2_scores['Catboost+RandomForest_gb'] = r2_2
+r2_scores['EnsembleModel_randomForest_XGB_CatBoost'] = r2_rf_xgb_cb
+
+# Combine predictions using averaging
+ensemble_predictions_avg = (y_pred_rf + y_pred_xgb + y_pred_cb) / 3
+ensemble_r2_avg = r2_score(y_test, ensemble_predictions_avg)
+
+# Update the scores dictionary with the ensemble R-squared
+r2_scores['Ensemble (Averaged)'] = ensemble_r2_avg
 
 # Select the model with the highest R-squared score
 best_model_name = max(r2_scores, key=r2_scores.get)
+best_model_r2 = r2_scores[best_model_name]
 
-# If the best model is the ensemble, we need to define it separately
-if best_model_name == 'Ensemble':
-    best_model = None  # Or you can define your ensemble model here
-else:
-    best_model = models[best_model_name]
+# Print R-squared scores of all models for comparison
+for model_name, r2_score in r2_scores.items():
+    print(f"{model_name}: R-squared = {r2_score}")
 
-# The Ensemble model is the bagging of random_forest+xGradientBoosting+Catboost Algorithm
-print("Best Model:", best_model_name)
-print("R-squared:", r2_scores[best_model_name])
+# Print the best model and its R-squared score
+print("\nBest Model:", best_model_name)
+print("R-squared:", best_model_r2)
+
+# Get the best model
+best_model = models[best_model_name] if best_model_name != 'Ensemble (Averaged)' else None
 
 
-input_data = (5,2.5,3650,9050,2,0,4,3,None,None,None,None,None,None,None,None)
+# In[73]:
 
-# change the input data to a numpy array
-input_data_as_numpy_array= np.asarray(input_data)
 
-# reshape the numpy array as we are predicting for only on instance
-input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
-
-prediction = best_model.predict(input_data_reshaped)
-print(prediction)
-
+import pickle 
 # Save the selected model as a pickle file
 with open('best_model.pkl', 'wb') as f:
     pickle.dump(best_model, f)
+
+
+# In[ ]:
+
+
+
+
